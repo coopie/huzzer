90c8459da1e8883af8243ca7fda9cf9a24ab1d17
Sam Coope <sam.j.coope@gmail.com>
2017-05-21 13:40:53 +0100

    Add more configurability to the main method

--- huzzer/huzz.py
+++ huzzer/huzz.py
@@ -2,12 +2,14 @@
 Huzz.
 
 Usage:
-    huzz [--seed=<randomseed>]
+    huzz [--seed=<randomseed>] [--max-args=<maxarglen>] [--expr-depth=<exprdepth>]
     huzz -h | --help
 
 Options:
-    -h --help               Show this screen.
-    -s --seed=<randomseed>  Use <randomseed> to initialise RNG.
+    -h --help                    Show this screen.
+    -s --seed=<randomseed>       Use <randomseed> to initialise RNG.
+    -a --max-args=<maxarglen>    Maximum number of arguments a function can have [default: 8].
+    -e --expr-depth=<exprdepth>  Maximum expression depth [default: 6].
 """
 from docopt import docopt
 from random import randint
@@ -19,9 +21,19 @@ from .function_generator import generate_functions
 from .code_gen import codeify_functions
 
 
-def huzzer(seed):
+def huzzer(
+    seed,
+    max_expression_depth=6,
+    max_number_of_functions=4,
+    max_type_signiature_length=8
+):
     # get random tree
-    functions = generate_functions(seed)
+    functions = generate_functions(
+        seed,
+        max_expression_depth=max_expression_depth,
+        max_number_of_functions=max_number_of_functions,
+        max_type_signiature_length=max_type_signiature_length
+    )
 
     # get naming generator
     namer = DefaultNamer()
@@ -31,6 +43,8 @@ def huzzer(seed):
 def main():
     arguments = docopt(__doc__, version=VERSION)
     randomseed_str = arguments.get('--seed')
+    maxarglen = int(arguments.get('--max-args'))
+    max_expression_depth = int(arguments.get('--expr-depth'))
 
     if randomseed_str is not None:
         try:
@@ -40,7 +54,11 @@ def main():
     else:
         randomseed = randint(0, maxsize)
 
-    print(huzzer(randomseed))
+    print(huzzer(
+        randomseed,
+        max_type_signiature_length=maxarglen + 1,
+        max_expression_depth=max_expression_depth
+    ))
 
 
 if __name__ == '__main__':
